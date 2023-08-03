ba9d6a80c6d4b055cae6ebe5f311d7272e262809
agpavlik <agpavlik@gmail.com>
2023-08-03 00:11:22 -0400

    Add calculation of packed items

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 
@@ -119,12 +119,24 @@ export default function App() {
     );
   }
 
-  function Stats() {
+  function Stats({ items }) {
+    if (!items.length)
+      return (
+        <p className="stats">
+          <em>Start adding some items to your packing list </em>
+        </p>
+      );
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
+
     return (
       <footer className="stats">
         <em>
-          You have ... items on your list, and you have already packed ...
-          (...%)
+          {percentage === 100
+            ? "You got everything! Ready to go ✈️"
+            : `You have ${numItems} items on your list, and you have already packed
+          ${numPacked} (${percentage}%)`}
         </em>
       </footer>
     );
